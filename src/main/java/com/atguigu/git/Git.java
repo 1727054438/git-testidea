b68d6397894317d174ff66fb16c7b5c25cd547bb
zhangsen <1727054438@qq.com>
2023-06-11 17:40:54 +0800

    third commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -11,5 +11,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
