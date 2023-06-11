6d5db4f3d43f66a4a125279fc2dc90b01c222b09
zhangsen <1727054438@qq.com>
2023-06-11 17:36:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * 功能描述:
+ *
+ * @author 张森
+ * @version 1.0
+ * @date 2023/6/11 17:30
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+}
